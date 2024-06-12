4b82812374a8227240240f4fd3b8a99af3b21229
MohamedSimar <87081914+MohamedSimar@users.noreply.github.com>
2024-06-12 12:37:18 +0530

    Create the function to write virtual mouse application module

--- main.py
+++ main.py
@@ -11,13 +11,63 @@ import pyautogui
 import json
 import fastdtw
 
+# Function to run the virtual mouse application
+def virtual_mouse_application():
+    cap = cv2.VideoCapture(0)
+    hand_detector = mp.solutions.hands.Hands()
+    drawing_utils = mp.solutions.drawing_utils
+    screen_width, screen_height = pyautogui.size()
 
+    index_y = 0
+
+    while True:
+        _, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+        frame_height, frame_width, _ = frame.shape
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        output = hand_detector.process(rgb_frame)
+        hands = output.multi_hand_landmarks
+        if hands:
+            for hand in hands:
+                drawing_utils.draw_landmarks(frame, hand)
+                landmarks = hand.landmark
+                for id, landmark in enumerate(landmarks):
+                    x = int(landmark.x * frame_width)
+                    y = int(landmark.y * frame_height)
+                    if id == 8:
+                        cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 255))
+                        index_x = screen_width / frame_width * x
+                        index_y = screen_height / frame_height * y
+
+                    if id == 4:
+                        cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 255))
+                        thumb_x = screen_width / frame_width * x
+                        thumb_y = screen_height / frame_height * y
+                        if abs(index_y - thumb_y) < 20:
+                            pyautogui.click()
+                            pyautogui.sleep(1)
+                        elif abs(index_y - thumb_y) < 100:
+                            pyautogui.moveTo(index_x, index_y)
+        cv2.imshow('Virtual Mouse', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    ##cut
+    # Set threads
+    show_camera_feed_thread = threading.Thread(target=show_camera_feed)
+    handle_user_input_thread = threading.Thread(target=handle_user_input)
+
+    # Start the threads
+    show_camera_feed_thread.start()
+    handle_user_input_thread.start()
+
+    # Join the threads to the main thread
+    show_camera_feed_thread.join()
+    handle_user_input_thread.join()
 
 def sinhala_sign_language_translator_application():
-
-
-  
-
     # A class to recognize gestures
     class GestureRecognizer:
         def init(self, detection_confidence, tracking_confidence):
